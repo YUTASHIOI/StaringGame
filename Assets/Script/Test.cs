afd121e85941978d41c595e54fba5331a8a3c998
DESKTOP-KKDEBRJ\ShibataRyuki <gunjyousky@gmail.com>
2019-06-05 22:17:18 +0900

    インテル入ってる

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -8,13 +8,26 @@ public class Test : MonoBehaviour
     void Start()
     {
         //test is test
+        Debug.Log("start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        while(1){
-        	// ‡
+        bool intel = true;
+
+        while(intel){
+            Debug.Log("intel“ü‚Á‚Ä‚éH");
+            Debug.Log(intel);
+
+            if (intel) Debug.Log("‚Í‚¢‚Á‚Ä‚È‚¢‚ñ‚©`‚¢");
+            else Debug.Log("‚Æ‚è‚„‚³‚µ‚Ü`‚·");
+
+            intel = false;
+            
+            if(!intel) Debug.Log("intelæ‚èo‚µ‚Ü‚µ‚½`");
+            Debug.Log("intelæ‚èo‚¹‚Ä‚È‚­‚È‚¢!?");
+            // ‡
         }
     }
 }
