06f2c5daceedcc38bf0ca9ec19f8e3dfb4615f18
tsuda0 <38852574+tsuda0@users.noreply.github.com>
2019-08-08 00:39:04 +0900

    「monkeyShapeScript」A～Zキー用変数作成

--- Assets/Resources/Script/monkeyShapeScript.cs
+++ Assets/Resources/Script/monkeyShapeScript.cs
@@ -6,12 +6,11 @@ public class monkeyShapeScript : MonoBehaviour
 {
 
 
-    public SkinnedMeshRenderer ref_MONKEY_DEF;
-    public float ratio_1 = 0.0f;    //変形度合い（0.0 ～ 100.0）
-    public float ratio_2 = 0.0f;    //変形度合い（0.0 ～ 100.0）
-    public float ratio_3 = 0.0f;    //変形度合い（0.0 ～ 100.0）
-    public float ratio_4 = 0.0f;    //変形度合い（0.0 ～ 100.0）
-    public float ratio_5 = 0.0f;    //変形度合い（0.0 ～ 100.0）
+    public SkinnedMeshRenderer faceMotion;
+    //変形度合い（0.0 ～ 100.0）
+    public float key_Q, key_W, key_E, key_R, key_T, key_Y, key_U, key_I, key_O, key_P;
+    public float key_A, key_S, key_D, key_F, key_G, key_H, key_J, key_K, key_L;
+    public float key_Z, key_X, key_C, key_V, key_B, key_N, key_M;
 
     [SerializeField]
     GameObject pMonkey;
@@ -20,7 +19,7 @@ public class monkeyShapeScript : MonoBehaviour
     void Start()
     {
         GameObject gMonkey = Instantiate(pMonkey) as GameObject;
-        ref_MONKEY_DEF = gMonkey.GetComponent<SkinnedMeshRenderer>();
+        faceMotion = gMonkey.GetComponent<SkinnedMeshRenderer>();
     }
 
     void Update()
@@ -31,58 +30,79 @@ public class monkeyShapeScript : MonoBehaviour
 
     void ShapeChange()
     {
-        ref_MONKEY_DEF.SetBlendShapeWeight(0, ratio_1);
-        //ref_MONKEY_DEF.SetBlendShapeWeight(1, ratio_2);
-        //ref_MONKEY_DEF.SetBlendShapeWeight(2, ratio_3);
-        //ref_MONKEY_DEF.SetBlendShapeWeight(3, ratio_4);
-        //ref_MONKEY_DEF.SetBlendShapeWeight(4, ratio_5);
+        faceMotion.SetBlendShapeWeight( 0, key_Q);
+        //faceMotion.SetBlendShapeWeight( 1, key_W);
+        //faceMotion.SetBlendShapeWeight( 2, key_E);
+        //faceMotion.SetBlendShapeWeight( 3, key_R);
+        //faceMotion.SetBlendShapeWeight( 4, key_T);
+        //faceMotion.SetBlendShapeWeight( 5, key_Y);
+        //faceMotion.SetBlendShapeWeight( 6, key_U);
+        //faceMotion.SetBlendShapeWeight( 7, key_I);
+        //faceMotion.SetBlendShapeWeight( 8, key_O);
+        //faceMotion.SetBlendShapeWeight( 9, key_P);
+        //faceMotion.SetBlendShapeWeight(10, key_A);
+        //faceMotion.SetBlendShapeWeight(11, key_S);
+        //faceMotion.SetBlendShapeWeight(12, key_D);
+        //faceMotion.SetBlendShapeWeight(13, key_F);
+        //faceMotion.SetBlendShapeWeight(14, key_G);
+        //faceMotion.SetBlendShapeWeight(15, key_H);
+        //faceMotion.SetBlendShapeWeight(16, key_J);
+        //faceMotion.SetBlendShapeWeight(17, key_K);
+        //faceMotion.SetBlendShapeWeight(18, key_L);
+        //faceMotion.SetBlendShapeWeight(19, key_Z);
+        //faceMotion.SetBlendShapeWeight(20, key_X);
+        //faceMotion.SetBlendShapeWeight(21, key_C);
+        //faceMotion.SetBlendShapeWeight(22, key_V);
+        //faceMotion.SetBlendShapeWeight(23, key_B);
+        //faceMotion.SetBlendShapeWeight(24, key_N);
+        //faceMotion.SetBlendShapeWeight(25, key_M);
     }
 
     void KeyInput()
     {
         if (Input.GetKey(KeyCode.Q))
         {
-            ratio_1 = RatioUp(ratio_1);
+            key_Q = RatioUp(key_Q);
         }
         else
         {
-            ratio_1 = RatioDown(ratio_1);
+            key_Q = RatioDown(key_Q);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            ratio_2 = RatioUp(ratio_2);
+            key_W = RatioUp(key_W);
         }
         else
         {
-            ratio_2 = RatioDown(ratio_2);
+            key_W = RatioDown(key_W);
         }
 
         if (Input.GetKey(KeyCode.O))
         {
-            ratio_3 = RatioUp(ratio_3);
+            key_O = RatioUp(key_O);
         }
         else
         {
-            ratio_3 = RatioDown(ratio_3);
+            key_O = RatioDown(key_O);
         }
 
         if (Input.GetKey(KeyCode.P))
         {
-            ratio_4 = RatioUp(ratio_4);
+            key_P = RatioUp(key_P);
         }
         else
         {
-            ratio_4 = RatioDown(ratio_4);
+            key_P = RatioDown(key_P);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            ratio_5 = RatioUp(ratio_5);
+            key_A = RatioUp(key_A);
         }
         else
         {
-            ratio_5 = RatioDown(ratio_5);
+            key_A = RatioDown(key_A);
         }
     }
 
