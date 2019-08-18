5f4fb44b89f523436e5e40d0f71fe2131af433f4
DESKTOP-KKDEBRJ\ShibataRyuki <gunjyousky@gmail.com>
2019-08-18 16:18:57 +0900

    顔のスケールの調整、ChopsticksControllerの追加

---
+++ Assets/Resources/Script/ChopsticksController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChopsticksController : MonoBehaviour
+{
+    [SerializeField] Transform chopstick_upper;
+    [SerializeField] Transform chopstick_bottom;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
