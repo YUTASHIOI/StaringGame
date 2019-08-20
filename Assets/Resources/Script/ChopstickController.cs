4b49df6a8d2c8289bcbdda7ee19f61b18da482b1
DESKTOP-KKDEBRJ\ShibataRyuki <gunjyousky@gmail.com>
2019-08-21 00:08:25 +0900

    ・箸の動きコントローラー

---
+++ Assets/Resources/Script/ChopstickController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChopstickController : MonoBehaviour
+{
+    public bool on_trigger;//モノに触れたかどうか
+    /*------------------------------------------------------------------*
+     * ◆箸がモノに触れたとき
+     *------------------------------------------------------------------*/
+    void OnTriggerEnter(Collider other)
+    {
+        on_trigger = true;
+    }
+    /*------------------------------------------------------------------*
+     * ◆箸がモノから離れたとき
+     *------------------------------------------------------------------*/
+    void OnTriggerExit(Collider other)
+    {
+        on_trigger = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        on_trigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Resources/Script/MouseController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public bool right_button;
+    public bool middle_button;
+    public bool left_button;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        right_button = false;
+        middle_button = false;
+        left_button = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) left_button = true;
+        if (Input.GetMouseButtonDown(1)) right_button = true;
+        if (Input.GetMouseButtonDown(2)) middle_button = true;
+        if (Input.GetMouseButtonUp(0))   left_button = false;
+        if (Input.GetMouseButtonUp(1))   right_button = false;
+        if (Input.GetMouseButtonUp(2))   middle_button = false;
+
+        Debug.Log(left_button + ":" + middle_button + ":" + right_button);
+
+    }
+}
