8325dc4697f68a2ba2815d51417b1f4e40b175d8
tsuda0 <38852574+tsuda0@users.noreply.github.com>
2019-09-01 17:54:09 +0900

    ３Dモデル変更

--- Assets/Resources/Script/monkeyShapeScript.cs
+++ Assets/Resources/Script/monkeyShapeScript.cs
@@ -10,12 +10,19 @@ public class monkeyShapeScript : MonoBehaviour
 
     [SerializeField, Range(0, 100)]
     //変形度合い（0.0 ～ 100.0）
-    public int key_Q, key_W, key_E, key_R, key_T, key_Y, key_U, key_I, key_O, key_P,
+    private int key_Q, key_W, key_E, key_R, key_T, key_Y, key_U, key_I, key_O, key_P,
                 key_A, key_S, key_D, key_F, key_G, key_H, key_J, key_K, key_L,
                 key_Z, key_X, key_C, key_V, key_B, key_N, key_M;
 
+    [SerializeField, Range(0, 100)]
+    //変形速度
+    private int transSpeed;
+
     [SerializeField]
-    GameObject pMonkey;
+    private GameObject pMonkey;
+
+    [SerializeField]
+    private bool key_Q_Switch, key_W_Switch, key_E_Switch, key_R_Switch, key_T_Switch, key_Y_Switch, key_U_Switch, key_I_Switch;
 
     // Start is called before the first frame update
     void Start()
@@ -26,20 +33,21 @@ public class monkeyShapeScript : MonoBehaviour
 
     void Update()
     {
-        KeyInput();
+        //KeyInput();
+        KeyInputSwitch();
         ShapeChange();
     }
 
     void ShapeChange()
     {
         faceMotion.SetBlendShapeWeight( 0, key_Q);          //右目閉じる
-        //faceMotion.SetBlendShapeWeight( 1, key_W);
-        //faceMotion.SetBlendShapeWeight( 2, key_E);
-        //faceMotion.SetBlendShapeWeight( 3, key_R);
-        //faceMotion.SetBlendShapeWeight( 4, key_T);
-        //faceMotion.SetBlendShapeWeight( 5, key_Y);
-        //faceMotion.SetBlendShapeWeight( 6, key_U);
-        //faceMotion.SetBlendShapeWeight( 7, key_I);
+        faceMotion.SetBlendShapeWeight( 1, key_W);
+        faceMotion.SetBlendShapeWeight( 2, key_E);
+        faceMotion.SetBlendShapeWeight( 3, key_R);
+        faceMotion.SetBlendShapeWeight( 4, key_T);
+        faceMotion.SetBlendShapeWeight( 5, key_Y);
+        faceMotion.SetBlendShapeWeight( 6, key_U);
+        faceMotion.SetBlendShapeWeight( 7, key_I);
         //faceMotion.SetBlendShapeWeight( 8, key_O);
         //faceMotion.SetBlendShapeWeight( 9, key_P);
         //faceMotion.SetBlendShapeWeight(10, key_A);
@@ -60,8 +68,12 @@ public class monkeyShapeScript : MonoBehaviour
         //faceMotion.SetBlendShapeWeight(25, key_M);
     }
 
+
+
+    //押し放し＝増減
     void KeyInput()
     {
+        //Qキー
         if (Input.GetKey(KeyCode.Q))
         {
             key_Q = RatioUp(key_Q);
@@ -71,6 +83,7 @@ public class monkeyShapeScript : MonoBehaviour
             key_Q = RatioDown(key_Q);
         }
 
+        //Wキー
         if (Input.GetKey(KeyCode.W))
         {
             key_W = RatioUp(key_W);
@@ -80,6 +93,67 @@ public class monkeyShapeScript : MonoBehaviour
             key_W = RatioDown(key_W);
         }
 
+        //Eキー
+        if (Input.GetKey(KeyCode.E))
+        {
+            key_E = RatioUp(key_E);
+        }
+        else
+        {
+            key_E = RatioDown(key_E);
+        }
+
+        //Rキー
+        if (Input.GetKey(KeyCode.R))
+        {
+            key_R = RatioUp(key_R);
+        }
+        else
+        {
+            key_R = RatioDown(key_R);
+        }
+
+        //Tキー
+        if (Input.GetKey(KeyCode.T))
+        {
+            key_T = RatioUp(key_T);
+        }
+        else
+        {
+            key_T = RatioDown(key_T);
+        }
+
+        //Yキー
+        if (Input.GetKey(KeyCode.Y))
+        {
+            key_Y = RatioUp(key_Y);
+        }
+        else
+        {
+            key_Y = RatioDown(key_Y);
+        }
+
+        //Uキー
+        if (Input.GetKey(KeyCode.U))
+        {
+            key_U = RatioUp(key_U);
+        }
+        else
+        {
+            key_U = RatioDown(key_U);
+        }
+
+        //Iキー
+        if (Input.GetKey(KeyCode.I))
+        {
+            key_I = RatioUp(key_I);
+        }
+        else
+        {
+            key_I = RatioDown(key_I);
+        }
+
+        //Oキー
         if (Input.GetKey(KeyCode.O))
         {
             key_O = RatioUp(key_O);
@@ -89,6 +163,7 @@ public class monkeyShapeScript : MonoBehaviour
             key_O = RatioDown(key_O);
         }
 
+        //Pキー
         if (Input.GetKey(KeyCode.P))
         {
             key_P = RatioUp(key_P);
@@ -98,6 +173,7 @@ public class monkeyShapeScript : MonoBehaviour
             key_P = RatioDown(key_P);
         }
 
+        //Aキー
         if (Input.GetKey(KeyCode.A))
         {
             key_A = RatioUp(key_A);
@@ -107,10 +183,106 @@ public class monkeyShapeScript : MonoBehaviour
             key_A = RatioDown(key_A);
         }
     }
+    
+    //押してる間変化------------------------
+    void KeyInputSwitch()
+    {
+        //Qキー
+        if (Input.GetKey(KeyCode.Q))
+        {
+            key_Q_Switch = UpOrDownSwitch(key_Q, key_Q_Switch);
+            key_Q = UpDown(key_Q, key_Q_Switch);
+        }
 
+        //Wキー
+        if (Input.GetKey(KeyCode.W))
+        {
+            key_W_Switch = UpOrDownSwitch(key_W, key_W_Switch);
+            key_W = UpDown(key_W, key_W_Switch);
+        }
+
+        //Eキー
+        if (Input.GetKey(KeyCode.E))
+        {
+            key_E_Switch = UpOrDownSwitch(key_E, key_E_Switch);
+            key_E = UpDown(key_E, key_E_Switch);
+        }
+
+        //Rキー
+        if (Input.GetKey(KeyCode.R))
+        {
+            key_R_Switch = UpOrDownSwitch(key_R, key_R_Switch);
+            key_R = UpDown(key_R, key_R_Switch);
+        }
+
+        //Tキー
+        if (Input.GetKey(KeyCode.T))
+        {
+            key_T_Switch = UpOrDownSwitch(key_T, key_T_Switch);
+            key_T = UpDown(key_T, key_T_Switch);
+        }
+
+        //Yキー
+        if (Input.GetKey(KeyCode.Y))
+        {
+            key_Y_Switch = UpOrDownSwitch(key_Y, key_Y_Switch);
+            key_Y = UpDown(key_Y, key_Y_Switch);
+        }
+
+        //Uキー
+        if (Input.GetKey(KeyCode.U))
+        {
+            key_U_Switch = UpOrDownSwitch(key_U, key_U_Switch);
+            key_U = UpDown(key_U, key_U_Switch);
+        }
+
+        //Iキー
+        if (Input.GetKey(KeyCode.I))
+        {
+            key_I_Switch = UpOrDownSwitch(key_I, key_I_Switch);
+            key_I = UpDown(key_I, key_I_Switch);
+        }
+    }
+
+    int UpDown(int key, bool Switch)
+    {
+        if (Switch)
+        {
+            key = RatioUp(key);
+        }
+        else
+        {
+            key = RatioDown(key);
+        }
+        return key;
+    }
+
+    bool UpOrDownSwitch(int key, bool Switch)
+    {
+        if (Switch)
+        {
+            if (key >= 100)
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if(key <= 0)
+            {
+                return true;
+            }
+        }
+        return Switch;
+    }
+
+    //--------------------------------------
+
+
+    //変形値増加
     int RatioUp(int ratio)
     {
-        ratio += 5;
+        ratio += transSpeed;
         if (ratio > 100)
         {
             ratio = 100;
@@ -118,9 +290,10 @@ public class monkeyShapeScript : MonoBehaviour
         return ratio;
     }
 
+    //変形値減少
     int RatioDown(int ratio)
     {
-        ratio -= 10;
+        ratio -= transSpeed;
         if (ratio < 0)
         {
             ratio = 0;
