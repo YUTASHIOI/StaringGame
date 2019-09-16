1894fed42c9f2556ba5f59e01a21393f5f1d8168
DESKTOP-KKDEBRJ\ShibataRyuki <gunjyousky@gmail.com>
2019-09-16 20:43:52 +0900

    ・箸の横移動を追加

---
+++ Assets/Resources/Script/ChopsticsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChopsticsController : MonoBehaviour
+{
+    [SerializeField]
+    float MoveRange = 30f;            //Z軸に対する箸の可動域
+    [SerializeField]
+    float Z_pos = -30f;            //Z軸に対する箸の可動域
+
+    private Vector3 pos;
+    // Start is called before the first frame update
+
+    /*----------------------------------------------------*
+     * ◆初期化関数
+     *----------------------------------------------------*/
+    void Init()
+    {
+        pos = new Vector3(0, 0, Z_pos);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.GetAxis("L_Vertical")+":"+Input.GetAxis("L_Vertical"));
+
+        pos.x = Input.GetAxis("L_Horizontal") * MoveRange;
+        pos.y = Input.GetAxis("L_Vertical")   *-MoveRange;
+        pos.z = Z_pos;
+        this.transform.localPosition = pos;
+    }
+}
