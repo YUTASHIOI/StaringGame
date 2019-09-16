634f814df1d33788731e21c794a6c110c9d220f4
DESKTOP-KKDEBRJ\ShibataRyuki <gunjyousky@gmail.com>
2019-09-16 17:42:16 +0900

    ・DOTween回転デモ

--- Assets/Resources/Script/CSRightRootController.cs
+++ Assets/Resources/Script/CSRightRootController.cs
@@ -15,6 +15,11 @@ public class CSRightRootController : MonoBehaviour
     void Update()
     {
         Debug.Log("hitt");
-        this.transform.DOMove(endValue: new Vector3(5.0f, 0.5f, 0), duration: 2.0f);
+        //this.transform.DOMove(endValue: new Vector3(5.0f, 0.5f, 0), duration: 2.0f);
+        // 1秒かけて90度まで回転
+        this.transform.DORotate(
+            new Vector3(0f, 0f, 30f), // 終了時点のRotation
+            10.0f                    // アニメーション時間
+        );
     }
 }
