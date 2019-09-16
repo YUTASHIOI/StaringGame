0483d801badfd598971a7524e33e0fb7e7d54c3f
DESKTOP-KKDEBRJ\ShibataRyuki <gunjyousky@gmail.com>
2019-09-16 20:58:52 +0900

    RLボタン操作の対応やり直し

--- Assets/Resources/Script/CSRightRootController.cs
+++ Assets/Resources/Script/CSRightRootController.cs
@@ -1,25 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 public class CSRightRootController : MonoBehaviour
 {
+    [SerializeField]
+    float Z_Range = 15f;            //Z軸に対する箸の可動域
+
+
+    /*----------------------------------------------------*
+     * ◆初期化関数
+     *----------------------------------------------------*/
+    void Init()
+    {
+        this.transform.localPosition = new Vector3(0f, 5f, 0f);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("hitt");
-        //this.transform.DOMove(endValue: new Vector3(5.0f, 0.5f, 0), duration: 2.0f);
-        // 1秒かけて90度まで回転
-        this.transform.DORotate(
-            new Vector3(0f, 0f, 30f), // 終了時点のRotation
-            10.0f                    // アニメーション時間
-        );
+        if (Input.GetButton("R2（デジタル）"))
+        {
+            this.transform.localRotation = Quaternion.Euler(
+                0f,
+                0f,
+                Z_Range * ((Input.GetAxis("R2（アナログ）") + 1f) / 2f)
+                );
+        }
+        else
+        {
+            this.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
     }
 }
