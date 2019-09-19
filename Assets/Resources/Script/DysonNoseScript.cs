d9e7e6d98ee682a33cccea7fcedd6680cb3de747
tsuda0 <38852574+tsuda0@users.noreply.github.com>
2019-09-20 01:20:14 +0900

    鼻吸い込み再々プレハブ化 眼球実装（不動）

---
+++ Assets/Resources/Script/DysonNoseScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DysonNoseScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Nose_Hole;
+
+    [SerializeField]
+    float power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "seed")
+        {
+            float dyson_power = power * Mathf.Sin(Time.time);
+            Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+            Vector3 nlz_vector = (other.transform.position - Nose_Hole.transform.position).normalized;
+            rigidbody.AddForce(nlz_vector * dyson_power);
+        }
+    }
+}
