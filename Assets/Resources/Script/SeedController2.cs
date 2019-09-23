344450666fa69cae8533e667baf2af1bd6f9fd12
tsuda0 <38852574+tsuda0@users.noreply.github.com>
2019-09-24 01:12:15 +0900

    顔サイズ修正、 SeedController2編集

--- Assets/Resources/Script/SeedController2.cs
+++ Assets/Resources/Script/SeedController2.cs
@@ -7,12 +7,15 @@ public class SeedController2 : MonoBehaviour
     [SerializeField]
     GameObject pSeed, Eye_R, Eye_L;
 
+    [SerializeField]
+    float Seed_Pos_def;
 
     // Start is called before the first frame update
     void Start()
     {
         Vector3 ER_Pos = Eye_R.transform.position;
-        GameObject gMonkey = Instantiate(pSeed, new Vector3(ER_Pos.x, ER_Pos.y, ER_Pos.z), Quaternion.identity) as GameObject;
+        Instantiate(pSeed, new Vector3(0.0f, 0.0f, -2.0f), Quaternion.identity);
+        //Instantiate(pSeed, new Vector3(ER_Pos.x, ER_Pos.y, ER_Pos.z - Seed_Pos_def), Quaternion.identity);
     }
 
     // Update is called once per frame
