8add439d6900f82fb6c9376cf1c9baf8eef189ea
tsuda0 <38852574+tsuda0@users.noreply.github.com>
2019-09-24 00:31:36 +0900

    SeedController2 作成

---
+++ Assets/Resources/Script/SeedController2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedController2 : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pSeed, Eye_R, Eye_L;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 ER_Pos = Eye_R.transform.position;
+        GameObject gMonkey = Instantiate(pSeed, new Vector3(ER_Pos.x, ER_Pos.y, ER_Pos.z), Quaternion.identity) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
