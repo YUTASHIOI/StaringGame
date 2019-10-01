e7f128925d6520d9e23c06faf0de87b5dc1d3eda
tsuda0 <38852574+tsuda0@users.noreply.github.com>
2019-10-02 01:29:27 +0900

    SeedController2　微修正

--- Assets/Resources/Script/SeedController2.cs
+++ Assets/Resources/Script/SeedController2.cs
@@ -16,16 +16,17 @@ public class SeedController2 : MonoBehaviour
         rb.AddForce(down, ForceMode.Impulse);
     }
 
+    Vector3 side1;
+    Vector3 side2;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 distance = Center_Pos - this.transform.position;
-        float magni = distance.sqrMagnitude;
-        Debug.Log("magni:" + magni);
-        if (magni > 80.0f)
-        {
-            rb.AddForce(distance/3, ForceMode.Force);
-        }
+        //Vector3 distance = Center_Pos - this.transform.position;
+        //float magni = distance.sqrMagnitude;
+        //Debug.Log("magni:" + magni);
+        side1 = Center_Pos - this.transform.position;
+        side2 = new Vector3(Center_Pos.x, Center_Pos.y, this.transform.position.z) - this.transform.position;
+        rb.AddForce(Vector3.Cross(side1, side2)/50, ForceMode.Force);
     }
 }
