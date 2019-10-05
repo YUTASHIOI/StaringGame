6e440544be01d8c67082d03c64919b904ac4e192
DESKTOP-KKDEBRJ\ShibataRyuki <gunjyousky@gmail.com>
2019-10-05 21:10:23 +0900

    ・カメラの描画範囲をピンク色で表示する

---
+++ Assets/Resources/Script/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public  float left;
+    public  float right;
+    public  float top;
+    public  float bottom;
+    public  float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LineRenderer renderer = gameObject.GetComponent<LineRenderer>();
+        // 線の幅
+        renderer.SetWidth(0.5f, 0.5f);
+        // 頂点の数
+        renderer.SetVertexCount(5);
+        // 頂点を設定
+        renderer.SetPosition(0, new Vector3(left, top, z));
+        renderer.SetPosition(1, new Vector3(right, top, z));
+        renderer.SetPosition(2, new Vector3(right, bottom, z));
+        renderer.SetPosition(3, new Vector3(left, bottom, z));
+        renderer.SetPosition(4, new Vector3(left, top, z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Resources/Script/ChopsticsController.cs
+++ Assets/Resources/Script/ChopsticsController.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class ChopsticsController : MonoBehaviour
 {
+    [SerializeField, TooltipAttribute("カメラの描画範囲")]
+    CameraController CameraController;
     [SerializeField, TooltipAttribute("箸モデルの大きさ")]
     public Vector3 chopstick;
     [SerializeField, TooltipAttribute("初期位置")]
-    private Vector3 init_pos;
-    [SerializeField, TooltipAttribute("箸の可動域")]
-    Vector2 move_range;
+    private Vector2 init_pos;
     [SerializeField, TooltipAttribute("移動速度")]
     float move_speed;
     [SerializeField, TooltipAttribute("移動量がnoize以下なら削除")]
     float noize;
 
 
+    private Vector3 CS_Right_Root;//箸本体の座標
+    private Vector3 CS_Left_Root;//箸本体の座標
     private Vector3 tmp_pos;    //移動量の仮置き
 
     /*----------------------------------------------------*
@@ -23,8 +25,14 @@ public class ChopsticsController : MonoBehaviour
      *----------------------------------------------------*/
     void Init()
     {
-        this.transform.localPosition = init_pos;
-        tmp_pos = init_pos;
+        //箸の初期位置設定
+        tmp_pos.x = init_pos.x;
+        tmp_pos.y = init_pos.y;
+        tmp_pos.z = CameraController.z;
+        this.transform.localPosition = tmp_pos;
+        //箸本体の座標取得
+        CS_Right_Root = gameObject.transform.Find("CSRight_Root").gameObject.transform.localPosition;
+        CS_Left_Root = gameObject.transform.Find("CSLeft_Root").gameObject.transform.localPosition;
     }
     // Start is called before the first frame update
     void Start()
@@ -39,23 +47,32 @@ public class ChopsticsController : MonoBehaviour
 
         if (Mathf.Abs(Input.GetAxis("L_Vertical")) + Mathf.Abs(Input.GetAxis("L_Vertical")) > noize)
         {
+            // 倒した角度によって移動を加速させたい
+
             //X軸
             tmp_pos.x += Input.GetAxis("L_Horizontal") * move_speed;
-            if (tmp_pos.x > move_range.x - chopstick.x) tmp_pos.x = move_range.x - chopstick.x;
-            else if (tmp_pos.x < -move_range.x) tmp_pos.x = -move_range.x;
+            if (tmp_pos.x > CameraController.right - chopstick.x) tmp_pos.x = CameraController.right - chopstick.x;
+            else if (tmp_pos.x < CameraController.left) tmp_pos.x = CameraController.left;
+
             //Y軸
             tmp_pos.y += Input.GetAxis("L_Vertical") * -move_speed;
-            if (tmp_pos.y > move_range.y + 2f) tmp_pos.y = move_range.y + 2f;
-            else if (tmp_pos.y < -move_range.y) tmp_pos.y = -move_range.y;
+            if (tmp_pos.y > CameraController.top - CS_Right_Root.y) tmp_pos.y = CameraController.top - CS_Right_Root.y;
+            else if (tmp_pos.y < CameraController.bottom - CS_Left_Root.y) tmp_pos.y = CameraController.bottom - CS_Left_Root.y;
+            
             //Z軸
-            tmp_pos.z = init_pos.z;
+            tmp_pos.z = CameraController.z;
+
 
             //コントローラーの値を代入する
             this.transform.localPosition = tmp_pos;
         }
         else
         {
-            this.transform.localPosition = init_pos;
+            //箸の初期位置設定
+            tmp_pos.x = init_pos.x;
+            tmp_pos.y = init_pos.y;
+            tmp_pos.z = CameraController.z;
+            this.transform.localPosition = tmp_pos;
         }
     }
 }
