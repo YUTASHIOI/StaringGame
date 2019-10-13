282e4034cc3c85463e448e141ee10d0265ed9b3e
tsuda0 <38852574+tsuda0@users.noreply.github.com>
2019-10-14 02:03:24 +0900

    FageShapeScript　SeedController2　微修正

--- Assets/Resources/Script/SeedController2.cs
+++ Assets/Resources/Script/SeedController2.cs
@@ -4,31 +4,7 @@ using UnityEngine;
 
 public class SeedController2 : MonoBehaviour
 {
-    //Vector3 Center_Pos = new Vector3(0.0f, 0.0f, 0.0f);
-    //
-    //public Rigidbody rb;
     Vector3 down = new Vector3(0.0f, -0.8f, 0.0f);
-    //
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    rb = GetComponent<Rigidbody>();
-    //    rb.AddForce(down, ForceMode.Impulse);
-    //}
-
-    //Vector3 side1;
-    //Vector3 side2;
-    //
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    //Vector3 distance = Center_Pos - this.transform.position;
-    //    //float magni = distance.sqrMagnitude;
-    //    //Debug.Log("magni:" + magni);
-    //    side1 = Center_Pos - this.transform.position;
-    //    side2 = new Vector3(Center_Pos.x, Center_Pos.y, this.transform.position.z) - this.transform.position;
-    //    rb.AddForce(Vector3.Cross(side1, side2)/50, ForceMode.Force);
-    //}
 
     public GameObject sun;
     public float initVelocityY;
--- Assets/Resources/Script/SeedDirector.cs
+++ Assets/Resources/Script/SeedDirector.cs
@@ -13,7 +13,7 @@ public class SeedDirector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InstanceSeed();
+
     }
 
     // Update is called once per frame
