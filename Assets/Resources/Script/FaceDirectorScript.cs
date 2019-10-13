949fe721b46e50db9dbcce4dcacc360a276fac8d
tsuda0 <38852574+tsuda0@users.noreply.github.com>
2019-10-14 01:44:28 +0900

    FaceDirector　追加

---
+++ Assets/Resources/Script/FaceDirectorScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceDirectorScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pFace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(pFace); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
