9198255f81963d9b31b433c34481ae92e2c869c6
Yuta Shioi <shiyu.25ar@gmail.com>
2019-11-03 20:42:06 +0900

    顔真似モード実装中＆FaceDirectorを元の形式に修正

--- Assets/Resources/ImitationResources/Scripts/ImitationFaceDirector.cs
+++ Assets/Resources/ImitationResources/Scripts/ImitationFaceDirector.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class ImitationFaceDirector : MonoBehaviour
 {
     [SerializeField]
-    GameObject pFace;
+    GameObject ImitationFace;
+    [SerializeField]
+    GameObject ImitationAutoFace;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(pFace,new Vector3(-15.0f,0.0f,0.0f), Quaternion.AngleAxis(-29.0f,Vector3.up));
+        Instantiate(ImitationAutoFace, new Vector3(-15.0f,0.0f,0.0f), Quaternion.AngleAxis(-29.0f,Vector3.up));
+        Instantiate(ImitationFace, new Vector3(15.0f, 0.0f, 0.0f), Quaternion.AngleAxis(21.0f, Vector3.up));
     }
 
     // Update is called once per frame
--- Assets/Resources/Script/FaceDirectorScript.cs
+++ Assets/Resources/Script/FaceDirectorScript.cs
@@ -7,18 +7,11 @@ public class FaceDirectorScript : MonoBehaviour
 {
     [SerializeField]
     GameObject pFace;
-    Vector3 FaceOriginPosition = new Vector3(0.0f, 0.0f, -2.5f);
-    Quaternion FaceOriginQuaternion = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
 
     // Start is called before the first frame update
     void Start()
     {
-        if(SceneManager.GetActiveScene().name == "ImitationGameScene")
-        {
-            FaceOriginPosition += new Vector3(10.0f,0.0f,0.0f);
-            FaceOriginQuaternion = Quaternion.AngleAxis(14.0f, Vector3.up);
-        }
-        GameObject Seed = Instantiate(pFace,FaceOriginPosition, Quaternion.identity) as GameObject;
+        GameObject Seed = Instantiate(pFace) as GameObject;
         //プレハブ名の変更
         Seed.name = "Face_Root";
     }
