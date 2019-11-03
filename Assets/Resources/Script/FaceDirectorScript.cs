7a1976188762a571239bb40fadb447693b4c7d5e
Yuta Shioi <shiyu.25ar@gmail.com>
2019-11-03 18:28:56 +0900

    顔の回転

--- Assets/Resources/Script/FaceDirectorScript.cs
+++ Assets/Resources/Script/FaceDirectorScript.cs
@@ -8,6 +8,7 @@ public class FaceDirectorScript : MonoBehaviour
     [SerializeField]
     GameObject pFace;
     Vector3 FaceOriginPosition = new Vector3(0.0f, 0.0f, -2.5f);
+    Quaternion FaceOriginQuaternion = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,7 @@ public class FaceDirectorScript : MonoBehaviour
         if(SceneManager.GetActiveScene().name == "ImitationGameScene")
         {
             FaceOriginPosition += new Vector3(10.0f,0.0f,0.0f);
+            FaceOriginQuaternion = Quaternion.AngleAxis(14.0f, Vector3.up);
         }
         Instantiate(pFace,FaceOriginPosition, Quaternion.identity);
     }
