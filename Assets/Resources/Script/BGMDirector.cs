d7f15d1327b5ce808a5085bf2d2f7f334a011e84
DESKTOP-KKDEBRJ\ShibataRyuki <gunjyousky@gmail.com>
2019-11-23 00:33:28 +0900

    BGMを追加

---
+++ Assets/Resources/Script/BGMDirector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMDirector : MonoBehaviour
+{
+    [SerializeField, TooltipAttribute("BGM1")]
+    AudioClip bgm_01;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //BGM再生
+        this.GetComponent<AudioSource>().PlayOneShot(bgm_01);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
